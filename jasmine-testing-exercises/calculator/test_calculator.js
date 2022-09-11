87057ece434a3fcd07745fa3e2be588b944281da
Victor Delgado <valexdelgado@gmail.com>
2022-09-10 20:41:53 -0700

    finished both tests as per exercise prompt, will add more tests tomorrow

--- jasmine-testing-exercises/calculator/test_calculator.js
+++ jasmine-testing-exercises/calculator/test_calculator.js
@@ -1,38 +1,55 @@
-const testValues = {
-  first: {
+const testValues = [
+  {
     amount: 10000, 
     years: 10,
-    rate: 4.5
+    rate: 4.5,
   },
-  second: {
+  {
     amount: 10, 
     years: 5,
-    rate: 1.5
+    rate: 1.5,
   },
-  third: {
+  {
     amount: 100, 
     years: 7,
-    rate: 2
+    rate: 2,
   },
-  fourth: {
+  {
     amount: 100000, 
     years: 10,
-    rate: 1
+    rate: 1,
+  }, 
+  {
+    amount: 1, 
+    years: 1,
+    rate: 1,
+  },
+  {
+    amount: 100, 
+    years: 100,
+    rate: 100,
+
   }
-}
+]
+
+const answers = [103.64, .17, 1.28, 876.04, .08, 8.33]
 
 
 
 it('should calculate the monthly rate correctly', function () {
-  expect(calculateMonthlyPayment(testValues.first)).toEqual(String(103.64))
-  expect(calculateMonthlyPayment(testValues.second)).toEqual(String(.17))
-  expect(calculateMonthlyPayment(testValues.third)).toEqual(String(1.28))
-  expect(calculateMonthlyPayment(testValues.fourth)).toEqual(String(876.04))
+  for(let i = 0; i < testValues.length; i++){
+    expect(calculateMonthlyPayment(testValues[i])).toEqual(String(answers[i]))
+  }
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  for(let i = 0; i < testValues.length; i++){
+    let answer = calculateMonthlyPayment(testValues[i])
+    let answer2DecPlaces = parseFloat(answer).toFixed(2)
+    expect(answer).toEqual(answer2DecPlaces)
+  }
+
 });
 
 /// etc
