200b3bea52f382e8c58d02c34e4c670b5a44855c
Victor Delgado <valexdelgado@gmail.com>
2022-09-24 11:54:28 -0700

    working on tests for tip calculator

--- jasmine-testing-exercises/tip-pool/servers.test.js
+++ jasmine-testing-exercises/tip-pool/servers.test.js
@@ -11,7 +11,27 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should handle an empty input', function(){
+    serverNameInput.value = ''
+    submitServerInfo()
+
+    expect(Object.keys(allServers).length).toEqual(0)
+    expect(JSON.stringify(Object.keys(allServers))).toEqual('[]')
+  })
+
+  it('update ServerTable', function(){
+    submitServerInfo()
+    updateServerTable()
+
+    let myTDlist = document.querySelectorAll('#serverTable tbody tr td')
+
+    expect(myTDlist[0].innerText).toEqual('Alice')
+    expect(myTDlist[1].innerText).toEqual('$0.00')
+  })
+
   afterEach(function() {
-    // teardown logic
+    serverId = 0
+    serverTbody.innerHTML = ''
+    allServers = {}
   });
 });
---
+++ jasmine-testing-exercises/tip-pool/test_helpers.js
@@ -0,0 +1,9 @@
+describe('testing the helper functions (with setup and teardown)', function(){
+    beforeEach(function () {
+        billAmtInput = 100
+        tipAmtInput = 15
+        submitPaymentInfo()
+    })
+
+it('')
+})
